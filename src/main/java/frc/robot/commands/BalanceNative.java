fe6a69713fbb86d619e16273167b97c0b5881928
Princess Nova <possiblynova@outlook.com>
2023-01-11 20:56:37 -0800

    pid command (native pid doesnt work)

--- src/main/java/frc/robot/commands/BalanceNative.java
+++ src/main/java/frc/robot/commands/BalanceNative.java
@@ -5,11 +5,11 @@ import frc.robot.subsystems.Drivetrain;
 
 // does not work correctly
 
-public class Balance extends CommandBase {
+public class BalanceNative extends CommandBase {
     private Drivetrain drivetrain;
     public double maxSpeed = 1;
 
-    public Balance(Drivetrain drivetrain) {
+    public BalanceNative(Drivetrain drivetrain) {
         this.addRequirements(drivetrain);
         this.drivetrain = drivetrain;
     }
@@ -31,10 +31,17 @@ public class Balance extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
-        this.drivetrain.setPIDSlot(1);
+        this.drivetrain.halt();
+        
+        this.drivetrain.setPIDSlot(0);
         this.drivetrain.setIntegratedSensors();
-        this.drivetrain.m_diffDrive.setSafetyEnabled(false);
+        this.drivetrain.m_diffDrive.setSafetyEnabled(true);
         
         this.drivetrain.halt();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
     }
 }
