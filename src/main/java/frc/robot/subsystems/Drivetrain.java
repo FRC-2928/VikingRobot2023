7e398a67a782c0dc623ed01446276215132ceef8
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2023-02-26 10:46:06 -0800

    Setup Shuffleboard tab

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -439,6 +439,9 @@ public class Drivetrain extends SubsystemBase {
 		}
 	}
 
+	// -----------------------------------------------------------
+	// Processing
+	// -----------------------------------------------------------
 	@Override
 	public void periodic() {
 		//if limelight sees april tags, use limelight odometry, otherwise update from pigeon and encoders
